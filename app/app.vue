2a067379b8c88edea655cf213d047dc40f9c391a
AkashAkter <akash31akter@gmail.com>
2024-11-02 12:28:10 +0600

    Project created

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>About</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quidem mollitia sit est provident libero
+            in labore necessitatibus? Facere nam perspiciatis voluptatem nisi eaque, provident tenetur possimus
+            distinctio repellat quibusdam?</p>
+    </div>
+</template>
+
+<script setup>
+// You can add any necessary logic here if needed
+</script>
+
+<style scoped>
+/* Add your scoped styles here */
+</style>
